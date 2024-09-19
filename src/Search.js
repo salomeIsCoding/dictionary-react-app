29e08f4d011c9f31099e6454e4de703e083ba508
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-09-19 18:19:19 +0200

    Created my Search Compenent

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
 export default function Search() {
-  return <p>Search</p>;
+  let [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword} definiton`);
+  }
+
+  return (
+    <div>
+      <form onSubmit={search}>
+        <input type="submit" value="" />
+        <input
+          type="search"
+          placeholder="search here"
+          autofocus={true}
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
 }
