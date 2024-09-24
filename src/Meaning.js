47626f16797108f10fd33f4859016d128a8ebbc1
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-09-24 18:02:59 +0200

    Rendered Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning.definition);
@@ -9,6 +10,7 @@ export default function Meaning(props) {
 
       <p>{props.meaning.definition}</p>
       <em>{props.meaning.example}</em>
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
