c9538c376545ac3adb5dd054c110569db93c17b7
salomeIsCoding <160015952+salomeIsCoding@users.noreply.github.com>
2024-09-24 19:28:22 +0200

    Imported Images

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Meaning from "./Meaning";
+import image2 from "./images/hearing.png";
+import "./Results.css";
 
 export default function Results(props) {
+  console.log(props.phonetics);
+  const imagesList = [
+    {
+      id: 2,
+      src: image2,
+      alt: "Image 2",
+    },
+  ];
+
   if (props.results) {
     return (
-      <div className="results">
-        <h2>{props.results.word}</h2>
+      <div className="results text-start">
+        <h2 className="mt-5">{props.results.word}</h2>
+        <div className="d-flex justify-content-start">
+          <div className="row">
+            <div className="col pe-0">
+              <p className="text-secondary">/{props.results.phonetic}/</p>
+            </div>
+            <div className="col ps-2">
+              {imagesList.map((image) => (
+                <img key={2} src={image2} alt="" height="30" />
+              ))}
+            </div>
+          </div>
+        </div>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
